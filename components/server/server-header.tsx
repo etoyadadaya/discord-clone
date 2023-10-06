7b2d275ae28882de70256e99c095bf2f199bd17f
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-06 08:57:56 +0700

    invitations

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
 import {ChevronDown, LogOut, PlusCircle, Settings, SettingsIcon, Trash, UserPlus, Users} from "lucide-react";
+import {useModal} from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -18,6 +19,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -39,6 +42,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       >
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("invite", { server })}
             className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
           >
             Invite People
